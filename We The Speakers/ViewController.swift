a1d3a53305c2c048721617310292a72bb312ff44
Shreeniket <shreeniketbendre@outlook.com>
2020-10-17 16:06:54 -0400

    Initial Commit

---
+++ We The Speakers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  We The Speakers
+//
+//  Created by Shreeniket Bendre on 10/17/20.
+//  Copyright © 2020 Shreeniket Bendre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
