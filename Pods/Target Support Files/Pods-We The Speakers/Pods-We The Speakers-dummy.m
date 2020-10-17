109a9fd4e816dff3599bccfc2ee1a97e1e1df331
sben05 <math8tube@gmail.com>
2020-10-17 16:41:44 -0400

    Added basic VC and Podfiles

---
+++ Pods/Target Support Files/Pods-We The Speakers/Pods-We The Speakers-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_We_The_Speakers : NSObject
+@end
+@implementation PodsDummy_Pods_We_The_Speakers
+@end
---
+++ We The Speakers/ViewControllers/Teleprompt/TeleCamViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TeleCamViewController.swift
+//  We The Speakers
+//
+//  Created by Shreeniket Bendre on 10/17/20.
+//  Copyright © 2020 Shreeniket Bendre. All rights reserved.
+//
+
+import UIKit
+
+class TeleCamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
