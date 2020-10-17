008925ab32eade1c8d173e9cbf0ba537c269aa2c
sben05 <math8tube@gmail.com>
2020-10-17 17:19:37 -0400

    Added Menu Navigation... IT ACTUALLY WORKS HAHA

--- We The Speakers/ViewControllers/MainViewController.swift
+++ We The Speakers/ViewControllers/MainViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import SideMenu
 
 class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
